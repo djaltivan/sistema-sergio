49324b6b06e46871aa95e69a263160643f45b39d
felix <djaltivan@yahoo.com.br>
2018-07-26 20:18:25 -0300

    segundo commit add funcao_expurgo_log

---
+++ main/funcao_expurgo_log.php
@@ -0,0 +1,7 @@
+﻿<?php
+ini_set( 'display_errors', false );
+error_reporting( 0 );
+include('conectar.php');
+mysql_query("DELETE FROM log WHERE datahora < DATE_SUB(NOW(), INTERVAL 6 MONTH);");
+?>
+Expurgo feito na tabela de logs
